fd7cbe22547aca1f6556d1e3054b10a9dc8ef6e7
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-13 14:43:02 +0600

    get by search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
 
 
 const App = () => {
+  const [search, setSearch] = useState('')
+  const [datas, setDatas] = useState('')
+
+  const handlesubmit = (e) => {
+    e.preventDefault()
+  fetch(`http://hn.algolia.com/api/v1/search?query=${search}`)
+    .then(res => res.json())
+    .then(data=> setDatas(data.hits))
+    
+  }
+  console.log(datas,'15');
   return (
-    <div>
-      <h1>hacker news</h1>
+    <div className="container mx-auto">
+      <div>
+        <div className="flex justify-center">
+          <div className="flex items-center mt-12">
+            <form onSubmit={handlesubmit}>
+        <input placeholder="search your posts" className="border-2 text-center w-96 h-12  border-black rounded text-xl" type="text" value={search} onChange={(e)=>setSearch(e.target.value)} name="" id="" />
+      <button className="px-8 ml-8 py-2 border-2 border-black rounded text-xl" type="submit">Search</button>
+      </form>
+          </div>
+        </div>
+        <h1 className="text-center my-10  font-bold text-3xl">{ datas ? "Hacker News": "Please search your title"}</h1>
+        <div className="grid grid-cols-4 gap-5">
+          {
+        datas &&
+        datas?.map((d, index) =>
+          <h2 className="bg-gray-100 px-4 py-1 w-80 h-16 flex justify-center items-center rounded hover:shadow-lg hover:shadow-gray-500" key={index}>{d.title ? d.title : d.story_title
+}</h2>)
+      }
+      </div>
+      
+      </div>
     </div>
   );
 };
--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Details from "./Details";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+  },
+  {
+    path: "/details/:id",
+    element: <Details/>,
   },
 ]);
